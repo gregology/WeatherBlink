c96d79c7749f2965d7bcacc42dc95f0f5f1cf16f
gregology <greg@gho.st>
2013-02-07 22:36:18 -0500

    Test config.rb gitignore

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # You can get a free wunder ground api key from http://www.wunderground.com/weather/api/
-$wundergroundapikey = '1122334455667788'
+$wundergroundapikey = '236d258e67fc286e'
 
 # city code, it took me a while to find this for Ottawa, if you find a list please push it to this git repo :)
 $citycode = 'zmw:00000.1.71063'
@@ -16,3 +16,4 @@ $warmcolour = '255,165,0'
 # What is hot? At what temp should the indicator turn red
 $hot = '15'
 $hotcolour = '255,0,0'
+
